8913fc7908239da36e4a18c89a8d46329c62320a
mher <mmher0091@gmail.com>
2024-05-09 14:52:47 +0300

    Add just for test

--- contracts/StableToken.sol
+++ contracts/StableToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+// Using just for test
 contract StableToken is ERC20("Stable Token", "ST") {
     function mint(address to, uint amount) external {
         _mint(to, amount);
